a2b5164438b6447530b05439ced7b1daeeea44af
Ryan Foster <fosterdavidryan@gmail.com>
2017-08-18 18:19:08 +0100

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,72 @@
+/**
+ * Created by truengine on 4/1/2017.
+ */
+
+/*
+
+function show() {
+    document.getElementById("a2").style.display = 'block';
+}
+function hide() {
+    document.getElementById("a2").style.display = 'none';
+}
+
+*/
+
+
+
+
+/*
+$(document).ready(function() {
+    function show() {
+        $("#a1").next().style.display = 'block';
+    }
+});
+
+*/
+
+
+
+
+/*
+    var a2= document.getElementById("a2"), b2= document.getElementById("b2"),
+        c2= document.getElementById("c2"),d2= document.getElementById("d2"),
+        e2= document.getElementById("e2"),f2= document.getElementById("e2"),
+        g2= document.getElementById("g2"),h2= document.getElementById("h2"),
+        i2= document.getElementById("i2"),j2= document.getElementById("j2"),
+        k2= document.getElementById("k2"), l2= document.getElementById("l2"),
+        m2= document.getElementById("m2"),n2= document.getElementById("n2"),
+        o2= document.getElementById("o2"),p2= document.getElementById("p2"),
+q2= document.getElementById("q2"),r2= document.getElementById("r2"),s2= document.getElementById("s2"),
+t2= document.getElementById("t2"), u2= document.getElementById("u2"),v2= document.getElementById("v2"),
+w2= document.getElementById("w2"),x2= document.getElementById("x2"),y2= document.getElementById("y2"),
+z2= document.getElementById("z2"),za2= document.getElementById("za2"),zb2= document.getElementById("zb2"),
+zc2= document.getElementById("zc2"), zd2= document.getElementById("zd2"),ze2= document.getElementById("ze2"),
+zf2= document.getElementById("zf2"),zg2= document.getElementById("zg2"),zh2= document.getElementById("zh2"),
+zi2= document.getElementById("zi2"),zj2= document.getElementById("zj2"),zk2= document.getElementById("zk2"),
+zl2= document.getElementById("zl2"), zm2= document.getElementById("zm2"),zn2= document.getElementById("zn2"),
+zo2= document.getElementById("zo2"),zp2= document.getElementById("zp2"), zq2= document.getElementById("zp2"),
+zr2= document.getElementById("zr2"),zs2= document.getElementById("zs2"),zt2= document.getElementById("zt2"),
+
+    a1= document.getElementById("a1"), b1= document.getElementById("b1"),
+    c1= document.getElementById("c1"),d1= document.getElementById("d1"),
+    e1= document.getElementById("e1"),f1= document.getElementById("f1"),
+    g1= document.getElementById("g1"),h1= document.getElementById("h1"),
+    i1= document.getElementById("i1"),j1= document.getElementById("j1"),
+    k1= document.getElementById("k1"), l1= document.getElementById("l1"),
+    m1= document.getElementById("m1"),n1= document.getElementById("n1"),
+    o1= document.getElementById("o1"),p1= document.getElementById("p1"),
+    q1= document.getElementById("q1"),r1= document.getElementById("r1"),s1= document.getElementById("s1"),
+    t1= document.getElementById("t1"), u1= document.getElementById("u1"),v1= document.getElementById("v1"),
+    w1= document.getElementById("w1"),x1= document.getElementById("x1"),y1= document.getElementById("y1"),
+    z1= document.getElementById("z1"),za1= document.getElementById("za1"),zb1= document.getElementById("zb1"),
+    zc1= document.getElementById("zc1"), zd1= document.getElementById("zd1"),ze1= document.getElementById("ze1"),
+    zf1= document.getElementById("zf1"),zg1= document.getElementById("zg1"),zh1= document.getElementById("zh1"),
+    zi1= document.getElementById("zi1"),zj1= document.getElementById("zj1"),zk1= document.getElementById("zk1"),
+    zl1= document.getElementById("zl1"), zm1= document.getElementById("zm1"),zn1= document.getElementById("zn1"),
+    zo1= document.getElementById("zo1"),zp1= document.getElementById("zp1"), zq1= document.getElementById("zp1"),
+    zr1= document.getElementById("zr1"),zs1= document.getElementById("zs1"),zt1= document.getElementById("zt1");
+*/
+
+
+
